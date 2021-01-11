9c39dfb64a8cdb0f521dbccfbe75fec1948735a1
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-01-11 12:03:16 +0100

    mostrando las citas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Fragment, useState} from 'react';
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
 
@@ -24,7 +25,13 @@ function App() {
           </div>
 
           <div className="one-half column">
-            2
+            <h2>Adminsitra tus Citas</h2>
+            {citas.map(cita => (
+              <Cita
+                key={cita.id}
+                cita={cita}
+              />
+            ))}
           </div>
         </div>
       </div>
